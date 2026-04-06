e8b43092da5706efa50905ee6ecd816fca8570ac
João Victor Lima Ewerton <joaoovewerton@gmail.com>
2026-04-06 13:11:00 -0300

    adiciona suporte a processamento em lote de diretórios

--- JackAnalyzer.java
+++ JackAnalyzer.java
@@ -2,31 +2,37 @@ import java.io.*;
 
 public class JackAnalyzer {
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Uso: java JackAnalyzer <arquivo.jack>");
-            return;
+        if (args.length != 1) return;
+
+        File fileOrDir = new File(args[0]);
+        File[] files;
+
+        if (fileOrDir.isFile()) files = new File[]{fileOrDir};
+        else files = fileOrDir.listFiles((dir, name) -> name.endsWith(".jack"));
+
+        if (files == null) return;
+
+        for (File file : files) {
+            try { processFile(file); } catch (Exception e) { }
         }
+    }
 
-        try {
-            File inputFile = new File(args[0]);
-            String outputFilename = inputFile.getAbsolutePath().replace(".jack", "T.xml");
-            PrintWriter writer = new PrintWriter(new File(outputFilename));
-            JackTokenizer tokenizer = new JackTokenizer(inputFile);
+    private static void processFile(File inputFile) throws IOException {
+        String outputFilename = inputFile.getAbsolutePath().replace(".jack", "T.xml");
+        PrintWriter writer = new PrintWriter(new File(outputFilename));
+        JackTokenizer tokenizer = new JackTokenizer(inputFile);
 
-            writer.println("<tokens>");
-            while (tokenizer.hasMoreTokens()) {
-                tokenizer.advance();
-                String tag = tokenizer.tokenType().toString().toLowerCase();
-                if (tag.equals("int_const")) tag = "integerConstant";
-                if (tag.equals("string_const")) tag = "stringConstant";
+        writer.println("<tokens>");
+        while (tokenizer.hasMoreTokens()) {
+            tokenizer.advance();
+            String tag = tokenizer.tokenType().toString().toLowerCase();
+            if (tag.equals("int_const")) tag = "integerConstant";
+            if (tag.equals("string_const")) tag = "stringConstant";
 
-                writer.println("<" + tag + "> " + tokenizer.getToken() + " </" + tag + ">");
-            }
-            writer.println("</tokens>");
-            writer.close();
-            System.out.println("Gerado com sucesso!");
-        } catch (Exception e) {
-            e.printStackTrace();
+            String val = tokenizer.getToken().replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;");
+            writer.println("<" + tag + "> " + val + " </" + tag + ">");
         }
+        writer.println("</tokens>");
+        writer.close();
     }
 }
