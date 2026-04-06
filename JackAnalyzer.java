f1fe519808a4535d18743c6b56643318525989fa
João Victor Lima Ewerton <joaoovewerton@gmail.com>
2026-04-06 13:35:35 -0300

    refina formatação do XML e altera arquivo de saída para T_Output.xml para facilitar a validação e evitar sobreposição de arquivo do projeto 10

--- JackAnalyzer.java
+++ JackAnalyzer.java
@@ -2,23 +2,36 @@ import java.io.*;
 
 public class JackAnalyzer {
     public static void main(String[] args) {
-        if (args.length != 1) return;
+        if (args.length != 1) {
+            System.out.println("Uso: java JackAnalyzer <arquivo_ou_diretorio>");
+            return;
+        }
 
         File fileOrDir = new File(args[0]);
         File[] files;
 
-        if (fileOrDir.isFile()) files = new File[]{fileOrDir};
-        else files = fileOrDir.listFiles((dir, name) -> name.endsWith(".jack"));
+        if (fileOrDir.isFile() && fileOrDir.getName().endsWith(".jack")) {
+            files = new File[]{fileOrDir};
+        } else if (fileOrDir.isDirectory()) {
+            files = fileOrDir.listFiles((dir, name) -> name.endsWith(".jack"));
+        } else {
+            System.out.println("Nenhum arquivo .jack encontrado.");
+            return;
+        }
 
-        if (files == null) return;
-
-        for (File file : files) {
-            try { processFile(file); } catch (Exception e) { }
+        if (files != null) {
+            for (File file : files) {
+                try {
+                    processFile(file);
+                } catch (IOException e) {
+                    System.out.println("Erro processando " + file.getName());
+                }
+            }
         }
     }
 
     private static void processFile(File inputFile) throws IOException {
-        String outputFilename = inputFile.getAbsolutePath().replace(".jack", "T.xml");
+        String outputFilename = inputFile.getAbsolutePath().replace(".jack", "T_Output.xml");
         PrintWriter writer = new PrintWriter(new File(outputFilename));
         JackTokenizer tokenizer = new JackTokenizer(inputFile);
 
@@ -26,13 +39,21 @@ public class JackAnalyzer {
         while (tokenizer.hasMoreTokens()) {
             tokenizer.advance();
             String tag = tokenizer.tokenType().toString().toLowerCase();
+            
             if (tag.equals("int_const")) tag = "integerConstant";
             if (tag.equals("string_const")) tag = "stringConstant";
 
-            String val = tokenizer.getToken().replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;");
+            String val = tokenizer.getToken();
+            
+            val = val.replace("&", "&amp;")
+                     .replace("<", "&lt;")
+                     .replace(">", "&gt;")
+                     .replace("\"", "&quot;");
+
             writer.println("<" + tag + "> " + val + " </" + tag + ">");
         }
         writer.println("</tokens>");
         writer.close();
+        System.out.println("Criado: " + outputFilename);
     }
 }
