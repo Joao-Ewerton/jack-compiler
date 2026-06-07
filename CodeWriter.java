8a9c828e06fc45d9a34d74153c837c0ebbc7b666
João Victor Lima Ewerton <joaoovewerton@gmail.com>
2026-06-07 14:32:31 -0300

    feat: suporte a leitura e escrita (push/pop) nos segmentos de memoria base e ponteiros

--- CodeWriter.java
+++ CodeWriter.java
@@ -18,23 +18,50 @@ public class CodeWriter {
     public void writeArithmetic(String command) {
         out.println("// " + command);
         if (command.equals("add") || command.equals("sub")) {
-            out.println("@SP\nAM=M-1\nD=M\nA=A-1"); // Desempilha 2 valores
+            out.println("@SP\nAM=M-1\nD=M\nA=A-1");
             if (command.equals("add")) out.println("M=M+D");
             if (command.equals("sub")) out.println("M=M-D");
         }
     }
 
     public void writePushPop(int command, String segment, int index) {
-        out.println("// push " + segment + " " + index);
-        if (command == Parser.C_PUSH && segment.equals("constant")) {
-            out.println("@" + index);
-            out.println("D=A");
-            // Coloca na pilha
+        out.println("// " + (command == Parser.C_PUSH ? "push" : "pop") + " " + segment + " " + index);
+        
+        if (command == Parser.C_PUSH) {
+            if (segment.equals("constant")) {
+                out.println("@" + index + "\nD=A");
+            } else if (isBaseSegment(segment)) {
+                out.println("@" + getSegmentPointer(segment) + "\nD=M\n@" + index + "\nA=D+A\nD=M");
+            } else if (segment.equals("temp")) {
+                out.println("@" + (5 + index) + "\nD=M");
+            } else if (segment.equals("pointer")) {
+                out.println("@" + (3 + index) + "\nD=M");
+            }
             out.println("@SP\nA=M\nM=D\n@SP\nM=M+1");
+
+        } else if (command == Parser.C_POP) {
+            if (isBaseSegment(segment)) {
+                out.println("@" + getSegmentPointer(segment) + "\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D");
+                out.println("@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D");
+            } else if (segment.equals("temp") || segment.equals("pointer")) {
+                out.println("@SP\nAM=M-1\nD=M");
+                if (segment.equals("temp")) out.println("@" + (5 + index));
+                if (segment.equals("pointer")) out.println("@" + (3 + index));
+                out.println("M=D");
+            }
         }
     }
 
-    public void close() {
-        if (out != null) out.close();
+    private boolean isBaseSegment(String seg) {
+        return seg.equals("local") || seg.equals("argument") || seg.equals("this") || seg.equals("that");
     }
+
+    private String getSegmentPointer(String seg) {
+        if (seg.equals("local")) return "LCL";
+        if (seg.equals("argument")) return "ARG";
+        if (seg.equals("this")) return "THIS";
+        return "THAT";
+    }
+
+    public void close() { if (out != null) out.close(); }
 }
