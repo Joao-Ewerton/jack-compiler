973154aeaea5d871ca622c28ee332719c24ca3a2
João Victor Lima Ewerton <joaoovewerton@gmail.com>
2026-06-07 14:34:02 -0300

    feat: adiciona operações relacionais (eq, gt, lt) com geração de labels dinamicos

--- CodeWriter.java
+++ CodeWriter.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 public class CodeWriter {
     private PrintWriter out;
     private String fileName;
+    private int labelCounter = 0;
 
     public CodeWriter(File file) {
         try { out = new PrintWriter(file); } 
@@ -17,28 +18,43 @@ public class CodeWriter {
 
     public void writeArithmetic(String command) {
         out.println("// " + command);
-        if (command.equals("add") || command.equals("sub")) {
+        if (command.equals("add") || command.equals("sub") || command.equals("and") || command.equals("or")) {
             out.println("@SP\nAM=M-1\nD=M\nA=A-1");
             if (command.equals("add")) out.println("M=M+D");
-            if (command.equals("sub")) out.println("M=M-D");
+            else if (command.equals("sub")) out.println("M=M-D");
+            else if (command.equals("and")) out.println("M=M&D");
+            else if (command.equals("or")) out.println("M=M|D");
+            
+        } else if (command.equals("neg") || command.equals("not")) {
+            out.println("@SP\nA=M-1");
+            if (command.equals("neg")) out.println("M=-M");
+            else if (command.equals("not")) out.println("M=!M");
+            
+        } else if (command.equals("eq") || command.equals("gt") || command.equals("lt")) {
+            String labelTrue = "TRUE_" + labelCounter;
+            String labelEnd = "END_" + labelCounter;
+            labelCounter++;
+
+            out.println("@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D");
+            out.println("@" + labelTrue);
+            if (command.equals("eq")) out.println("D;JEQ");
+            else if (command.equals("gt")) out.println("D;JGT");
+            else if (command.equals("lt")) out.println("D;JLT");
+
+            out.println("@SP\nA=M-1\nM=0\n@" + labelEnd + "\n0;JMP"); // False
+            out.println("(" + labelTrue + ")\n@SP\nA=M-1\nM=-1"); // True
+            out.println("(" + labelEnd + ")");
         }
     }
 
     public void writePushPop(int command, String segment, int index) {
         out.println("// " + (command == Parser.C_PUSH ? "push" : "pop") + " " + segment + " " + index);
-        
         if (command == Parser.C_PUSH) {
-            if (segment.equals("constant")) {
-                out.println("@" + index + "\nD=A");
-            } else if (isBaseSegment(segment)) {
-                out.println("@" + getSegmentPointer(segment) + "\nD=M\n@" + index + "\nA=D+A\nD=M");
-            } else if (segment.equals("temp")) {
-                out.println("@" + (5 + index) + "\nD=M");
-            } else if (segment.equals("pointer")) {
-                out.println("@" + (3 + index) + "\nD=M");
-            }
+            if (segment.equals("constant")) out.println("@" + index + "\nD=A");
+            else if (isBaseSegment(segment)) out.println("@" + getSegmentPointer(segment) + "\nD=M\n@" + index + "\nA=D+A\nD=M");
+            else if (segment.equals("temp")) out.println("@" + (5 + index) + "\nD=M");
+            else if (segment.equals("pointer")) out.println("@" + (3 + index) + "\nD=M");
             out.println("@SP\nA=M\nM=D\n@SP\nM=M+1");
-
         } else if (command == Parser.C_POP) {
             if (isBaseSegment(segment)) {
                 out.println("@" + getSegmentPointer(segment) + "\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D");
@@ -52,16 +68,12 @@ public class CodeWriter {
         }
     }
 
-    private boolean isBaseSegment(String seg) {
-        return seg.equals("local") || seg.equals("argument") || seg.equals("this") || seg.equals("that");
-    }
-
+    private boolean isBaseSegment(String seg) { return seg.equals("local") || seg.equals("argument") || seg.equals("this") || seg.equals("that"); }
     private String getSegmentPointer(String seg) {
         if (seg.equals("local")) return "LCL";
         if (seg.equals("argument")) return "ARG";
         if (seg.equals("this")) return "THIS";
         return "THAT";
     }
-
     public void close() { if (out != null) out.close(); }
 }
