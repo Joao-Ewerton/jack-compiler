9f5a580c9187076b26afcd5f1af3092af6234720
João Victor Lima Ewerton <joaoovewerton@gmail.com>
2026-06-22 16:31:55 -0300

    feat: adiciona suporte a processamento de diretórios e injeção automática de bootstrap

--- VMTranslator.java
+++ VMTranslator.java
@@ -7,56 +7,60 @@ public class VMTranslator {
             return;
         }
 
-        File inputFile = new File(args[0]);
-        
-        if (inputFile.isFile() && inputFile.getName().endsWith(".vm")) {
-            processFile(inputFile);
-        } else if (inputFile.isDirectory()) {
-            File[] files = inputFile.listFiles((dir, name) -> name.endsWith(".vm"));
-            if (files != null) {
-                for (File file : files) {
-                    processFile(file);
+        File input = new File(args[0]);
+        File outputFile;
+        File[] vmFiles;
+
+        // Verifica se a entrada é um diretório ou um arquivo único
+        if (input.isDirectory()) {
+            outputFile = new File(input, input.getName() + ".asm");
+            vmFiles = input.listFiles((dir, name) -> name.endsWith(".vm"));
+        } else {
+            outputFile = new File(input.getAbsolutePath().replace(".vm", ".asm"));
+            vmFiles = new File[]{input};
+        }
+
+        if (vmFiles == null || vmFiles.length == 0) {
+            System.out.println("Nenhum arquivo .vm encontrado.");
+            return;
+        }
+
+        CodeWriter codeWriter = new CodeWriter(outputFile);
+
+        if (input.isDirectory()) {
+            codeWriter.writeInit();
+        }
+
+        for (File vmFile : vmFiles) {
+            System.out.println("Traduzindo: " + vmFile.getName());
+            codeWriter.setFileName(vmFile.getName());
+            Parser parser = new Parser(vmFile);
+
+            while (parser.hasMoreCommands()) {
+                parser.advance();
+                int type = parser.commandType();
+
+                if (type == Parser.C_ARITHMETIC) {
+                    codeWriter.writeArithmetic(parser.arg1());
+                } else if (type == Parser.C_PUSH || type == Parser.C_POP) {
+                    codeWriter.writePushPop(type, parser.arg1(), parser.arg2());
+                } else if (type == Parser.C_LABEL) {
+                    codeWriter.writeLabel(parser.arg1());
+                } else if (type == Parser.C_GOTO) {
+                    codeWriter.writeGoto(parser.arg1());
+                } else if (type == Parser.C_IF) {
+                    codeWriter.writeIf(parser.arg1());
+                } else if (type == Parser.C_FUNCTION) {
+                    codeWriter.writeFunction(parser.arg1(), parser.arg2());
+                } else if (type == Parser.C_CALL) {
+                    codeWriter.writeCall(parser.arg1(), parser.arg2());
+                } else if (type == Parser.C_RETURN) {
+                    codeWriter.writeReturn();
                 }
             }
-        } else {
-            System.out.println("Caminho inválido.");
         }
-    }
-
-    private static void processFile(File file) {
-        String outputPath = file.getAbsolutePath().replace(".vm", ".asm");
-        Parser parser = new Parser(file);
-        CodeWriter codeWriter = new CodeWriter(new File(outputPath));
-        codeWriter.setFileName(file.getName());
-
-        System.out.println("Traduzindo: " + file.getName());
-
-        while (parser.hasMoreCommands()) {
-            parser.advance();
-            int type = parser.commandType();
-
-            if (type == Parser.C_ARITHMETIC) {
-                codeWriter.writeArithmetic(parser.arg1());
-            } else if (type == Parser.C_PUSH || type == Parser.C_POP) {
-                codeWriter.writePushPop(type, parser.arg1(), parser.arg2());
-            } 
-            // Novos roteamentos - Controle de Fluxo
-            else if (type == Parser.C_LABEL) {
-                codeWriter.writeLabel(parser.arg1());
-            } else if (type == Parser.C_GOTO) {
-                codeWriter.writeGoto(parser.arg1());
-            } else if (type == Parser.C_IF) {
-                codeWriter.writeIf(parser.arg1());
-            }
-            //Novos roteamentos - Sub-rotinas
-            else if (type == Parser.C_FUNCTION) {
-                codeWriter.writeFunction(parser.arg1(), parser.arg2());
-            } else if (type == Parser.C_CALL) {
-                codeWriter.writeCall(parser.arg1(), parser.arg2());
-            } else if (type == Parser.C_RETURN) {
-                codeWriter.writeReturn();
-            }
-        }
+        
         codeWriter.close();
+        System.out.println("Tradução concluída: " + outputFile.getAbsolutePath());
     }
 }
